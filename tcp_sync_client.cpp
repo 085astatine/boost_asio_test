938a3d54bd8c388862ec45c916c77eb5d18809a0
Astatine <astatine085@gmail.com>
2016-05-11 23:59:46 +0900

    Tutorial Daytime.1&2を補足修正
    コメント,出力を追加.
    boost::posix_time, std::arrayに変更.

--- tcp_sync_client.cpp
+++ tcp_sync_client.cpp
@@ -1,5 +1,5 @@
+#include <array>
 #include <iostream>
-#include <boost/array.hpp>
 #include <boost/asio.hpp>
 
 using boost::asio::ip::tcp;
@@ -11,15 +11,32 @@ int main(int argc, char* argv[]){
             return 1;
         }
         boost::asio::io_service io_service;
+        // Server名をparameterで指定しTCP endpointとする
+        // tcp::resolver: queryを受け取り Endpoint Listとする
         tcp::resolver resolver(io_service);
+        // Query作成
+        //   name of server : argv[1]
+        //   name of service: daytime
         tcp::resolver::query query(argv[1], "daytime");
+        // EndPoint List: io::tcp::resolver::iterator として返される
+        //   iteratorのdefault constructorはend iteratorとなる
         tcp::resolver::iterator endpoint_iterator = resolver.resolve(query);
+        // create &connect Socket
         tcp::socket socket(io_service);
+        // EndPoint ListにはIPv4とIPv6が混在している可能性
+        //   boost::asio::connectionはそれを自動的に解決する
         boost::asio::connect(socket, endpoint_iterator);
         while(true){
-            boost::array<char, 128> buf;
+            // hold Received Data
+            //   char[], std::vector等で代替可能
+            // boost::asio::buffer
+            //   Buffer Overrunが起こらないよう自動的にsizeを決定する
+            std::array<char, 128> buf;
             boost::system::error_code error;
-            size_t len = socket.read_some(boost::asio::buffer(buf), error);
+            // Close Connection
+            // socket::read_some boost::asio::error::eof_errorで終了
+            const std::size_t len = socket.read_some(
+                        boost::asio::buffer(buf), error);
             if (error == boost::asio::error::eof){
                 break; // Connection closed cleanly by peer.
             }else if(error){
--- tcp_sync_server.cpp
+++ tcp_sync_server.cpp
@@ -1,25 +1,42 @@
-#include <ctime>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <boost/asio.hpp>
-
-using boost::asio::ip::tcp;
+#include <boost/date_time/posix_time/posix_time.hpp>
 
 std::string make_daytime_string(){
-    std::time_t now = std::time(nullptr);
-    return std::ctime(&now);
+    const boost::posix_time::ptime ptime =
+                boost::posix_time::microsec_clock::local_time();
+    std::ostringstream sout;
+    sout << ptime << std::endl;
+    return sout.str();
 }
 
 int main(){
+    using boost::asio::ip::tcp;
+    std::cout << "Server Start: " << make_daytime_string();
     try{
         boost::asio::io_service io_service;
+        // io::tcp::adapter
+        //  TCP IPv4 Port 13
+        //  Iterative Server: 1 connection at once
         tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 13));
         while(true){
+            std::cout << "Iterate Start: " << make_daytime_string();
+            // Socket: ClientとのConnectionを表す
             tcp::socket socket(io_service);
+            // Wait Connectiont
             acceptor.accept(socket);
-            std::string message = make_daytime_string();
+            std::cout << "Get Socket   : " << make_daytime_string();
+            const std::string message = make_daytime_string();
+            std::cout << "Message      : " << message;
             boost::system::error_code ignored_error;
-            boost::asio::write(socket, boost::asio::buffer(message), ignored_error);
+            // Send Message to Client
+            boost::asio::write(
+                        socket,
+                        boost::asio::buffer(message),
+                        ignored_error);
+            std::cout << "Iterate End  : " << make_daytime_string();
         }
     }catch(std::exception& e){
         std::cerr << e.what() << std::endl;
