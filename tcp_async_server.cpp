492bfb5fbf4315be7273d2044c156c12010eec45
Astatine <astatine085@gmail.com>
2016-05-15 17:29:59 +0900

    Tutorial Daytime.3を補足
    Comment,Streeam出力を追加.

--- tcp_async_server.cpp
+++ tcp_async_server.cpp
@@ -29,6 +29,9 @@ private:
     boost::asio::ip::tcp::acceptor acceptor_;
 };
 
+// TCP Connection
+//  use shared_ptr, enable_shared_from_this
+//  to keep the Object alive as long as there is an Operation that refers to it
 class TCPConnection: public std::enable_shared_from_this<TCPConnection>{
 public:
     // pointer type
@@ -36,6 +39,8 @@ public:
     // Create
     static pointer create(
                 boost::asio::io_service& io_service);
+    // destructor
+    ~TCPConnection();
     // Socket
     boost::asio::ip::tcp::socket& socket();
     // Start
@@ -55,6 +60,7 @@ private:
 };
 // TCPServer
 // constructor
+//   Initialize: Acceptor to listen on TCP port 13
 TCPServer::TCPServer(
             boost::asio::io_service& io_service)
       : acceptor_(io_service,
@@ -64,6 +70,7 @@ TCPServer::TCPServer(
     start_accept();
 }
 // Start Accept
+//   Create Socket & Initialize Asynchronous Accept Operation
 void TCPServer::start_accept(){
     auto new_connection = TCPConnection::create(
                 acceptor_.get_io_service());
@@ -75,18 +82,26 @@ void TCPServer::start_accept(){
                 });
 }
 // Handle Accept
+//   Called when Asynchronous Accept Operation Initialized
 void TCPServer::handle_accept(
             const std::shared_ptr<TCPConnection>& new_connection,
             const boost::system::error_code& error){
     if(! error){
+        // Service: the Client Request
+        std::cout << "Call: TCPConnection::start()" << std::endl;
         new_connection->start();
     }
+    // Initialize the next Accept Operation
+    std::cout << "Create New Connection" << std::endl;
     start_accept();
 }
 // TCPConnection
 // Create
 TCPConnection::pointer TCPConnection::create(
             boost::asio::io_service& io_service){
+    // 関数内class
+    //   std::make_sharedの関数内ではprivate constructorを呼び出せないため.
+    //   newで生成するならば不要.
     class Dummy: public TCPConnection{
     public:
         explicit Dummy(boost::asio::io_service& io_service)
@@ -97,15 +112,30 @@ TCPConnection::pointer TCPConnection::create(
 // constructor
 TCPConnection::TCPConnection(
             boost::asio::io_service& io_service)
-      : socket_(io_service){}
+      : socket_(io_service){
+    std::cout << "TCP Connection constructed: at "
+              << make_daytime_string();
+}
+// destructor
+TCPConnection::~TCPConnection(){
+    std::cout << "TCP Connection destructed : at "
+              << make_daytime_string();
+}
 // Socket
 boost::asio::ip::tcp::socket& TCPConnection::socket(){
     return socket_;
 }
 // Start
+//   Call boost::async_write() to serve the data to the client
+//   boost::async_write_some() ではなく boost::async_writeを使う理由
+//     to ensure that the entire block of data is sent
 void TCPConnection::start(){
     const auto self = shared_from_this();
+    // The Data to be sent:
+    //   class member
+    //   as we need keep it valid until the Asynchronous Operation is Completed
     message_ = make_daytime_string();
+    // Capture shared_ptr
     boost::asio::async_write(
                 socket_,
                 boost::asio::buffer(message_),
@@ -117,9 +147,10 @@ void TCPConnection::start(){
 // Handle Write
 void TCPConnection::handle_write(
             const boost::system::error_code&,
-            std::size_t){
+            std::size_t bytes_transferred){
+    std::cout << bytes_transferred
+              << " bytes Transferred" << std::endl;
 }
-
 
 int main(){
     std::cout << "TCP Asynchronous Server" << std::endl;
